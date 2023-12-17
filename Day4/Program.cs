2007ec1cff817c7d795ec15ded83c4fd91fb33a6
Benoît Guillemaud <benoit@guillemaud.fr>
2023-12-17 01:21:43 +0100

    Day4

---
+++ Day4/Program.cs
@@ -0,0 +1,29 @@
+﻿using Day4;
+
+using (var file = File.OpenText("input.txt"))
+{
+    List<ScratchCard> cards = [];
+
+    String? line = null;
+    while ((line = file.ReadLine()) != null)
+    {
+        cards.Add(new ScratchCard(line));
+    }
+
+    double Part1 = cards.Sum(c => c.Points);
+
+    Console.WriteLine("Part 1 : " + Part1);
+
+    for (int i = 0; i < cards.Count; i++)
+    {
+        var card = cards[i];
+
+        for (int j = 1; j <= card.WinningCount; j++) // Should probably add an out of bounds check...
+        {
+            cards[i + j].Copy(card.Copies);
+        }
+    }
+
+    int Part2 = cards.Sum(c => c.Copies);
+    Console.WriteLine("Part 2 : " + Part2);
+}
---
+++ Day4/ScratchCard.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal class ScratchCard
+    {
+        readonly List<int> _winning;
+        readonly List<int> _got;
+        readonly int _ID;
+        private int _copies;
+
+        public int Copies => _copies;
+        public int WinningCount => _got.Where(g => _winning.Any(w => w == g)).Count();
+        public double Points
+        {
+            get
+            {
+                if (WinningCount == 0) return 0;
+                else return Math.Pow(2, WinningCount - 1);
+            }
+        }
+
+        public ScratchCard(String input)
+        {
+            _copies = 1;
+            String pattern = @"^Card +(\d+): ([\d\s]+) \| ([\d\s]+)$";
+
+            Match match = Regex.Match(input, pattern);
+            if (match.Success)
+            {
+                _ID = int.Parse(match.Groups[1].Value);
+
+                _winning = [];
+                foreach (String number in match.Groups[2].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    _winning.Add(int.Parse(number));
+                }
+
+                _got = [];
+                foreach (String number in match.Groups[3].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    _got.Add(int.Parse(number));
+                }
+            }
+            else
+            {
+                throw new Exception("Could not parse the following input : " + input);
+            }
+        }
+
+        public void Copy()
+        {
+            _copies++;
+        }
+        public void Copy(int num)
+        {
+            _copies += num;
+        }
+    }
+}
