2cc9b63b324a2a61d18cd0dbe21b60be27748268
Benoît Guillemaud <benoit@guillemaud.fr>
2023-12-17 16:31:33 +0100

    Day5 (still dirty and Part2 is really slow)

---
+++ Day5/Map.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class Map
+    {
+        readonly MapType _type;
+        List<Range> _ranges = [];
+
+        Boolean IsInSrcRange(uint x)
+        {
+            return _ranges
+                .Any(r => r.SourceStart <= x
+                && (r.SourceStart + r.Length - 1) >= x
+            );
+        }
+
+        public uint Convert(uint x)
+        {
+            if (!IsInSrcRange(x))
+            {
+                return x;
+            }
+            else
+            {
+                Range range = _ranges
+                    .Where(r => r.SourceStart <= x
+                    && (r.SourceStart + r.Length - 1) >= x)
+                    .First();
+
+                return x + (range.DestStart - range.SourceStart);
+            }
+        }
+
+        public Map(MapType type) { _type = type; }
+
+        public void AddRange(Range range) { _ranges.Add(range); }
+        public void AddRange(uint src, uint dest, uint len)
+        {
+            var r = new Range()
+            {
+                SourceStart = src,
+                DestStart = dest,
+                Length = len
+            };
+            this._ranges.Add(r);
+        }
+
+        public enum MapType
+        {
+            SeedToSoil,
+            SoilToFertilizer,
+            FertilizerToWater,
+            WaterToLight,
+            LightToTemperature,
+            TemperatureToHumidity,
+            HumidityToLocation
+        };
+        public struct Range
+        {
+            public uint SourceStart;
+            public uint DestStart;
+            public uint Length;
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Text.RegularExpressions;
+using Day5;
+using static Day5.Map;
+
+const int MAPCOUNT = 7;
+
+List<String> input = [];
+List<uint> seeds = [];
+Dictionary<MapType,Map> maps = [];
+
+// initializing maps dict
+for (int x = 0; x < MAPCOUNT; x++)
+{
+    MapType t = (MapType)x;
+    maps.Add(t, new(t));
+}
+
+// Loading the input file
+using (var file = File.OpenText("input.txt"))
+{
+    String? line = null;
+    while ((line = file.ReadLine()) != null)
+    {
+        input.Add(line);
+    }
+}
+
+// Seeds numbers init (for Part 1)
+Match m = Regex.Match(input[0], @"seeds: ([\d\s]+)");
+if (m.Success)
+{
+    foreach(String seedNumber in m.Groups[1].Value.Split(' '))
+    {
+        seeds.Add(uint.Parse(seedNumber));
+    }
+}
+else
+{
+    throw new Exception("Could not parse seeds numbers");
+}
+
+
+
+// Start at the seed to soil map
+int i = 3; //Data starts on the fourth line of the input file
+MapType type = MapType.SeedToSoil;
+
+while (i < input.Count)
+{
+    if (input[i] == String.Empty)   // When changing map (empty line in input)
+    {
+        type += 1;                  // Increment the MapType
+        i += 2;                     // Jump to start of data of next map, skipping header
+        continue;                   // Next loop
+    }
+    
+    // Parse the input
+    String[] map = input[i].Split(' ');
+    uint dest_start = uint.Parse(map[0]);
+    uint source_start = uint.Parse(map[1]);
+    uint length = uint.Parse(map[2]);
+    // Add the parsed info into a new range in the current map
+    maps[type].AddRange(source_start, dest_start, length);
+
+    i++;
+}
+
+
+
+// Looking for the nearest location (Part 1)
+Console.WriteLine("Nearest location 1 : " + NearestLocation(seeds, maps));
+
+// Seeds numbers init (for Part 1)
+var seeds2 = new List<(uint, uint)>();
+foreach (Match match in Regex.Matches(input[0], @"(\d+\s\d+)"))
+{
+    String[] seedRange = match.Groups[1].Value.Split(' ');
+
+    uint rangeStart = uint.Parse(seedRange[0]);
+    uint rangeLen = uint.Parse(seedRange[1]);
+
+    seeds2.Add((rangeStart, rangeLen));
+}
+// Looking for the nearest location (Part 2)
+Console.WriteLine("Nearest location 2 : " + NearestLocation2(seeds2, maps));
+
+static uint NearestLocation(List<uint> seeds, Dictionary<MapType, Map> maps)
+{
+    uint location = uint.MaxValue;
+    foreach (uint seed in seeds)
+    {
+        uint value = seed;
+        for (int x = 0; x < MAPCOUNT; x++)
+        {
+            value = maps[(MapType)x].Convert(value);
+        }
+
+        if (value < location) { location = value; }
+    }
+    return location;
+}
+
+static uint NearestLocation2(List<(uint, uint)> seeds, Dictionary<MapType, Map> maps)
+{
+    uint location = uint.MaxValue;
+    foreach ((uint start, uint length) in seeds)
+    {
+        for (uint seed = start; seed< start + length; seed++) {
+            uint value = seed;
+            for (int x = 0; x < MAPCOUNT; x++)
+            {
+                value = maps[(MapType)x].Convert(value);
+            }
+
+            if (value < location) { location = value; }
+        }
+    }
+    return location;
+}
