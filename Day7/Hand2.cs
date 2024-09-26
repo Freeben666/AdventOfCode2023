30423997a1bc2032aaacedb8f969c5afc57370a1
GUILLEMAUDBenoitZeendoc <b.guillemaud@zeendoc.com>
2024-09-26 19:33:23 +0200

    Give the correct answer for Day7 Part2 !

--- Day7/Hand2.cs
+++ Day7/Hand2.cs
@@ -9,101 +9,89 @@ public class Hand2 : IComparable<Hand2>
         public int Bid;
         public HandType Type;
 
+        private int _jokers;
+        private int _pairs;
+        private int _trips;
+        private int _quads;
+        private int _quints;
+        private Dictionary<char, int> _cardCounts = new Dictionary<char, int>();
+
     public Hand2(string cards, int bid)
     {
         Cards = cards ?? throw new ArgumentNullException(nameof(cards));
         Bid = bid;
 
-        this.SetType();
+        // Count the number of each kind of cards.
+        foreach (var card in Cards)
+        {
+            if (card == 'J') {
+                _jokers++;
+                continue;
+            }
+            if (!_cardCounts.ContainsKey(card)) _cardCounts[card] = 0;
+            _cardCounts[card]++;
+        }
+
+        _pairs = _cardCounts.Values.Where(x => x == 2).Count();
+        _trips = _cardCounts.Values.Where(x => x == 3).Count();
+        _quads = _cardCounts.Values.Where(x => x == 4).Count();
+        _quints = _cardCounts.Values.Where(x => x == 5).Count();
+
+        // Determine the type of hand.
+        if (_cardCounts.Count() == 0){
+            if (_jokers == 5) Type = HandType.FiveOfAKind;
+            else throw new Exception("Impossible hand !");
+        }
+        else if (_cardCounts.Values.Max() + _jokers >= 5) Type = HandType.FiveOfAKind;
+        else if (_cardCounts.Values.Max() + _jokers == 4) Type = HandType.FourOfAKind;
+        else if (
+            (_trips >0 && _pairs >0) ||
+            (_trips > 0 && _jokers > 0) || //Should already be Quads, but whatever
+            (_pairs == 2 && _jokers == 1)
+        ) Type = HandType.FullHouse;
+        else if (_cardCounts.Values.Max() + _jokers == 3) Type = HandType.ThreeOfAKind;
+        else if (_pairs == 2) Type = HandType.TwoPair;
+        else if (_cardCounts.Values.Max() + _jokers == 2) Type = HandType.OnePair;
+        else Type = HandType.HighCard;
     }
 
-    public void SetType()
+    public int CompareTo(Hand2? other)
+    {
+        if (other == null) return -1; // If the other hand is null, this hand is greater.
+        
+        if ((int)this.Type < (int)other.Type) return -1;
+        if ((int)this.Type > (int)other.Type) return 1;
+
+        var thisCards = Cards.ToList();
+        var otherCards = other.Cards.ToList();
+
+        for (int i = 0; i < thisCards.Count; i++)
         {
-            var cardCounts = new Dictionary<char, int>();
-            var jokers = 0;
-
-            foreach (var card in Cards)
+            if (CardValue(thisCards[i]) < CardValue(otherCards[i]))
             {
-                if (card == 'J') {
-                    jokers++;
-                    continue;
-                }
-                if (!cardCounts.ContainsKey(card)) cardCounts[card] = 0;
-                cardCounts[card]++;
+                //Console.WriteLine("{0} < {1}",this.Cards, other.Cards);
+                return -1;
             }
-
-            var pairs = cardCounts.Values.Where(x => x == 2).Count();
-            var threes = cardCounts.Values.Where(x => x == 3).Count();
-            var fours = cardCounts.Values.Where(x => x == 4).Count();
-            var fives = cardCounts.Values.Where(x => x == 5).Count();
-
-            if (
-                fives > 0 ||
-                (fours > 0 && jokers > 0) ||
-                (threes > 0 && jokers > 1) ||
-                (pairs > 0 && jokers > 2) ||
-                jokers > 3
-            ) Type = HandType.FiveOfAKind;
-            else if (
-                fours > 0 ||
-                (threes > 0 && jokers > 0) ||
-                (pairs > 0 && jokers > 1) ||
-                jokers > 2
-            ) Type = HandType.FourOfAKind;
-            else if (
-                (threes == 1 && pairs > 0) ||
-                threes > 1
-            ) Type = HandType.FullHouse;
-            else if (
-                threes > 0 ||
-                (pairs > 0 && jokers > 0) ||
-                jokers > 1
-            ) Type  = HandType.ThreeOfAKind;
-            else if (pairs > 1) Type = HandType.TwoPair;
-            else if (
-                pairs > 0 ||
-                jokers >0
-            ) Type = HandType.OnePair;
-            else Type = HandType.HighCard;
+            if (CardValue(thisCards[i]) > CardValue(otherCards[i]))
+            {
+                //Console.WriteLine("{0} > {1}",this.Cards, other.Cards);
+                return 1;
+            }
         }
 
-        public int CompareTo(Hand2? other)
+        return 0;
+    }
+
+    private int CardValue(char card)
+    {
+        switch (card)
         {
-            if (other == null) return -1; // If the other hand is null, this hand is greater.
-            
-            if ((int)this.Type < (int)other.Type) return -1;
-            if ((int)this.Type > (int)other.Type) return 1;
-
-            var thisCards = Cards.ToList();
-            var otherCards = other.Cards.ToList();
-
-            for (int i = 0; i < thisCards.Count; i++)
-            {
-                if (CardValue(thisCards[i]) < CardValue(otherCards[i]))
-                {
-                    //Console.WriteLine("{0} < {1}",this.Cards, other.Cards);
-                    return -1;
-                }
-                if (CardValue(thisCards[i]) > CardValue(otherCards[i]))
-                {
-                    //Console.WriteLine("{0} > {1}",this.Cards, other.Cards);
-                    return 1;
-                }
-            }
-
-            return 0;
-        }
-
-        private int CardValue(char card)
-        {
-            switch (card)
-            {
-                case 'A': return 14;
-                case 'K': return 13;
-                case 'Q': return 12;
-                case 'J': return 1;
-                case 'T': return 10;
-                default: return card - '0';
-            }
+            case 'A': return 14;
+            case 'K': return 13;
+            case 'Q': return 12;
+            case 'J': return 1;
+            case 'T': return 10;
+            default: return card - '0';
         }
     }
+}
