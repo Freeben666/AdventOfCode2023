95505e984ddc93d94a9db6a9fbde39665984a4fd
Benoît Guillemaud <benoit@guillemaud.fr>
2024-09-26 16:46:41 +0200

    Début de partie 2, mais ça bug

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -3,11 +3,13 @@ using Day7;
 
 var lines = File.ReadAllLines("input.txt");
         var hands = new List<Hand>();
+        var hands2 = new List<Hand2>();
 
         foreach (var line in lines)
         {
             var parts = line.Split(' ');
             hands.Add(new Hand( parts[0],int.Parse(parts[1]) ));
+            hands2.Add(new Hand2( parts[0],int.Parse(parts[1]) ));
         }
 
         hands.Sort();
@@ -19,3 +21,13 @@ var lines = File.ReadAllLines("input.txt");
         }
 
         Console.WriteLine($"Total winnings: {totalWinnings}");
+
+        hands2.Sort();
+
+        int totalWinnings2 = 0;
+        for (int i = 0; i < hands2.Count; i++)
+        {
+            totalWinnings2 += hands2[i].Bid * (i + 1);
+        }
+
+        Console.WriteLine($"Total winnings using Jokers: {totalWinnings2}");
