34a9ae81c7e11121c4524e9ca0c37f11e87c780a
GUILLEMAUDBenoitZeendoc <b.guillemaud@zeendoc.com>
2024-09-27 20:19:52 +0200

    Much faster part2 using LCM

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,4 +1,5 @@
-﻿using Day8;
+﻿using System.Text.RegularExpressions;
+using Day8;
 
 var input = File.ReadAllLines("input.txt");
 var instructions = input[0];
@@ -17,65 +18,77 @@ Console.WriteLine($"Nodes read: {nodes.Count()}");
 
 
 int steps = 0;
-/*
-string currentNode = "AAA";
-
-while (currentNode != "ZZZ") {
-    for (int i = 0; i < instructions.Length; i++) {
-        steps++;
-
-        var instruction = instructions[i];
-        
-        switch (instruction) {
-            case 'L':
-               currentNode = nodes[currentNode].Left;
-               break;
-            case 'R':
-               currentNode = nodes[currentNode].Right;
-               break;
-            default:
-               throw new Exception("Invalid instruction");
-        }
-
-        if (currentNode == "ZZZ") {
-            break;
-        }
-    }
-}
+steps = traverseGraph(nodes, instructions, "AAA", "ZZZ");
 
 Console.WriteLine($"Steps taken for part 1: {steps}");
-*/
+
 
 var currentNodes = nodes.Keys.Where( k => k[2] == 'A').ToList();
 var a = currentNodes.Count();
-steps = 0;
 
 Console.WriteLine($"Nodes that end with 'A': {a}");
 
-while (currentNodes.Where(x => x[2] == 'Z').Count() != a) {
-    //Console.WriteLine(currentNodes.Where(x => x[2] == 'Z').Count());
-    for (int i = 0; i < instructions.Length; i++) {
-        steps++;
+var steps2 = new Stack<ulong>();
 
-        var instruction = instructions[i];
-        
-        for (int j = 0; j < currentNodes.Count(); j++) {
+foreach (var node in currentNodes) {
+    steps2.Push((ulong)traverseGraph(nodes, instructions, node, @"^[0-9A-Z]{2}Z$"));
+}
+
+ulong x = steps2.Pop();
+
+foreach (var step in steps2){
+    x = lcm(x, step);
+}
+
+Console.WriteLine($"Steps taken for part 2: {x}");
+
+// Functions
+
+static int traverseGraph(Dictionary<String, Node> nodes, string instructions, string startNode, string endNodePattern){
+    var currentNode = startNode;
+    var steps = 0;
+
+    Regex r = new Regex(endNodePattern);
+
+    while (!r.Match(currentNode).Success) {
+        for (int i = 0; i < instructions.Length; i++) {
+            steps++;
+
+            var instruction = instructions[i];
+            
             switch (instruction) {
                 case 'L':
-                currentNodes[j] = nodes[currentNodes[j]].Left;
+                currentNode = nodes[currentNode].Left;
                 break;
                 case 'R':
-                currentNodes[j] = nodes[currentNodes[j]].Right;
+                currentNode = nodes[currentNode].Right;
                 break;
                 default:
                 throw new Exception("Invalid instruction");
             }
-        }
 
-        if (currentNodes.Where(x => x[2] == 'Z').Count() == a) {
-            break;
+            if (r.Match(currentNode).Success) {
+                break;
+            }
         }
+    }
+
+    return steps;
+}
+
+
+static ulong gcd(ulong n1, ulong n2)
+{
+    if (n2 == 0)
+    {
+        return n1;
+    }
+    else
+    {
+        return gcd(n2, n1 % n2);
     }
 }
 
-Console.WriteLine($"Steps taken for part 2: {steps}");
+static ulong lcm(ulong a, ulong b) {
+    return (a * b) / gcd(a,b);
+}
