da6d58c11cc7c5fcdee4a9597ae966a5a21c88bb
Benoît Guillemaud <benoit@guillemaud.fr>
2024-10-09 18:47:16 +0200

    Day10

---
+++ Day10/Program.cs
@@ -0,0 +1,8 @@
+﻿char[,] grid;
+
+var input = File.ReadAllLines("input.txt");
+
+int columns = input[0].Length; // L'axe des X fait la longueur d'une ligne du fichier d'entrée
+int rows = input.Length; // L'axe des Y fait la hauteur du fichier d'entrée
+
+grid = new char[rows,columns];
